6e95e2201ee60b94de16009d8dd6f7c4f77bceee
EvgeniiK <evgenkuznetso@gmail.com>
2016-06-30 14:17:10 +0300

    login system

--- parser.rb
+++ parser.rb
@@ -2,8 +2,10 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'sanitize'
+require 'mechanize'
+require 'watir-webdriver'
 
-# Parser task
+# Scraping
 class Parser
   def from(url)
     Nokogiri::HTML(open(url), nil, 'UTF-8')
@@ -36,8 +38,33 @@ class Parser
     end
     concat(titles, articles)
   end
+
+  def hero_page
+    puts Sanitize.clean(page_after_login.search("#hero_lastitems"))
+  end
+
+  # Login in, and returns page
+  def page_after_login
+    agent = Mechanize.new
+    page = agent.get('http://godville.net/login')
+    form = page.forms.first
+    form['username'] = 'lordlenoks@rambler.ru'
+    form['password'] = 'orochimaru'
+    pp page = agent.submit(form, form.buttons.first)
+    pp page = agent.get('http://godville.net/login')
+
+  end
+
+  def watirz
+    browser = Watir::Browser.new :firefox
+    browser.goto('http://bit.ly/watir-example')
+  end
+
 end
 
 p = Parser.new
-puts p.opinion
-p.blog_news
+# puts p.opinion
+# p.blog_news
+#p.page_after_login
+#p.hero_page
+puts p.watirz
